8b321d3b487630af114948530303fc50d9121c3c
Alexis Fossart <alexis.fossart@insa-lyon.fr>
2018-01-12 15:01:15 +0100

    initialisation of repository

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 8887:8888 -v ./notebooks:/base/notebooks -v ./data:/base/data -d deep
+docker run -p 8887:8888 -v notebooks:/base/notebooks -v data:/base/data -d deep
